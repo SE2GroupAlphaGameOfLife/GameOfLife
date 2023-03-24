a77f5baeb05c86c31947138be2295fdb2f9e55cb
tinesime <60291334+tinesime@users.noreply.github.com>
2023-03-24 11:45:57 +0100

    Databinding integrated

--- GameOfLife/app/src/main/java/com/example/gameoflife/MainActivity.java
+++ GameOfLife/app/src/main/java/com/example/gameoflife/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.gameoflife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.view.WindowInsets;
 import android.view.WindowManager;
 
+import com.example.gameoflife.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        
 
     }
 }
