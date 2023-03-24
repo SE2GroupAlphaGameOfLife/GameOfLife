4b5ec4b9ce226ab0601421221d5d611bf769c05a
tinesime <60291334+tinesime@users.noreply.github.com>
2023-03-24 11:32:22 +0100

    Uses internet permissions, app fullscreen

--- GameOfLife/app/src/main/java/com/example/gameoflife/MainActivity.java
+++ GameOfLife/app/src/main/java/com/example/gameoflife/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gameoflife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
