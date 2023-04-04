f0a9298dd6b578b4c4760b8d1df39ee6c703e118
moritzkupper <moritz.kupper.mk@gmail.com>
2023-04-04 20:53:51 +0200

    created libGDX project

---
+++ libGDX/android/src/com/aau/groupalpha_gameoflife/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.aau.groupalpha_gameoflife;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.aau.groupalpha_gameoflife.GameOfLife;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameOfLife(), config);
+	}
+}
