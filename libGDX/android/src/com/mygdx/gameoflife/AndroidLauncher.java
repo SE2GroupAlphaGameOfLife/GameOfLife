349d98546c52b6d9905ced00775b8a888af2b4ba
niwiunig <niwiunig@edu.aau.at>
2023-04-08 01:23:29 +0200

    Added Dependencies

--- libGDX/android/src/com/mygdx/gameoflife/AndroidLauncher.java
+++ libGDX/android/src/com/mygdx/gameoflife/AndroidLauncher.java
@@ -1,17 +1,15 @@
-package com.aau.groupalpha_gameoflife;
+package com.mygdx.gameoflife;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.aau.groupalpha_gameoflife.GameOfLife;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		//We use GameOfLife.getInstance() instead of a Constructor because it is a Singleton
 		initialize(GameOfLife.getInstance(), config);
 	}
 }
