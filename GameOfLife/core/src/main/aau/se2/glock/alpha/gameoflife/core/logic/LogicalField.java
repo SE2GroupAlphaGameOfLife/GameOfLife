b0184b22317d899b14776c93bd289a6c36b55961
Michael <mischuchnigg@edu.aau.at>
2023-04-27 16:01:39 +0200

    added logic classes stubs

---
+++ GameOfLife/core/src/main/aau/se2/glock/alpha/gameoflife/core/logic/LogicalField.java
@@ -0,0 +1,27 @@
+package aau.se2.glock.alpha.gameoflife.core.logic;
+
+import aau.se2.glock.alpha.gameoflife.core.GameField;
+
+public class LogicalField {
+
+    boolean isSpecial;
+
+    SpecialEvent specialEvent; //is null if field NOT special
+    boolean hasPlayer;
+
+    int position;
+
+    GameField field;
+
+    public LogicalField(GameField field){
+        this.field = field;
+
+    }
+
+
+
+
+
+
+
+}
