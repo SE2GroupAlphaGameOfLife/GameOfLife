c9fde7f0b0cebdacdaa645f9d7ba3e6fd3f9a173
lukita69 <122468559+lukita69@users.noreply.github.com>
2023-05-09 04:59:37 +0200

    Added private modifiers to attributes and added needed getter / setter

--- GameOfLife/core/src/test/aau/se2/glock/alpha/gameoflife/core/EventCardsTest.java
+++ GameOfLife/core/src/test/aau/se2/glock/alpha/gameoflife/core/EventCardsTest.java
@@ -1,7 +1,6 @@
 package aau.se2.glock.alpha.gameoflife.core;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,22 +17,22 @@ public class EventCardsTest {
     Card c1;
     Event e1, e2, e3, e4;
     Stack s1;
-    ArrayList <Card> cardList;
-    ArrayList <Event> eventList1;
+    ArrayList<Card> cardList;
+    ArrayList<Event> eventList1;
     EventData f1;
 
 
     @Before
-    public void setup(){
-    c1 = new Card();
-    cardList = new ArrayList<>();
-    eventList1 = new ArrayList<>();
-    s1 = new Stack();
-    e1 = new Event(100,0,"Erhalte 100 LP.");
-    e2 = new Event(200,0,"Erhalte 200 LP.");
-    e3 = new Event(0,1000,"Erhalte € 1.000.");
-    e4 = new Event(0,2000,"Erhalte € 2.000.");
-    f1 = new EventData();
+    public void setup() {
+        c1 = new Card();
+        cardList = new ArrayList<>();
+        eventList1 = new ArrayList<>();
+        s1 = new Stack();
+        e1 = new Event(100, 0, "Erhalte 100 LP.");
+        e2 = new Event(200, 0, "Erhalte 200 LP.");
+        e3 = new Event(0, 1000, "Erhalte € 1.000.");
+        e4 = new Event(0, 2000, "Erhalte € 2.000.");
+        f1 = new EventData();
 
     }
 
@@ -41,24 +40,24 @@ public class EventCardsTest {
      * Test for EventData method FillData.
      */
     @Test
-    public void testFillData(){
-        assertEquals(0,f1.eventList.size());
+    public void testFillData() {
+        assertEquals(0, f1.getEventList().size());
         f1.fillEventList();
-        assertEquals(100,f1.eventList.size());
+        assertEquals(100, f1.getEventList().size());
     }
 
     /**
      * Test for EventData method FillCardList.
      */
     @Test
-    public void testFillCardList(){
+    public void testFillCardList() {
         f1.fillEventList();
         f1.fillCardList();
-        assertEquals(f1.eventList.size()/4,f1.cardList.size());
+        assertEquals(f1.getEventList().size() / 4, f1.getCardList().size());
     }
 
     @Test
-    public void testGetEvent(){
+    public void testGetEvent() {
         f1.fillEventList();
         f1.fillCardList();
         c1.fillEvents();
@@ -66,31 +65,31 @@ public class EventCardsTest {
     }
 
     @Test
-    public void testFillEvents(){
+    public void testFillEvents() {
         f1.fillEventList();
         f1.fillCardList();
         //assertEquals(4,c1.fillEvents.size());
     }
 
     @Test
-    public void testGetTopCard(){
+    public void testGetTopCard() {
 
     }
 
     @Test
-    public void testMixCards(){
-    c1.addEvent(0,e1);
-    c1.addEvent(1,e2);
-    c1.addEvent(2,e3);
-    c1.addEvent(3,e4);
-    cardList.add(c1);
-    s1.mixCards();
-    //assertNotEquals(e1,c1.getEvent(0));
+    public void testMixCards() {
+        c1.addEvent(0, e1);
+        c1.addEvent(1, e2);
+        c1.addEvent(2, e3);
+        c1.addEvent(3, e4);
+        cardList.add(c1);
+        s1.mixCards();
+        //assertNotEquals(e1,c1.getEvent(0));
 
     }
 
     @Test
-    public void testAddCards(){
+    public void testAddCards() {
 
     }
 }
