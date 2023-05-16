ed4ad5db7c4634be8c8f9dc2594fa2a0cd45da0f
lukita69 <122468559+lukita69@users.noreply.github.com>
2023-05-16 13:01:38 +0200

    Adapted Main AndroidLauncher to handle lifecycle and initialize sensorListener

--- GameOfLife/android/src/aau/se2/glock/alpha/gameoflife/AndroidLauncher.java
+++ GameOfLife/android/src/aau/se2/glock/alpha/gameoflife/AndroidLauncher.java
@@ -6,12 +6,31 @@ import android.view.WindowManager;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
+import aau.se2.glock.alpha.gameoflife.sensor.SensorListener;
+
 public class AndroidLauncher extends AndroidApplication {
+
+	private SensorListener sensorListener;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		sensorListener = new SensorListener(this, null);
+		GameOfLife.proximitySensorInterface = sensorListener;
 		initialize(GameOfLife.getInstance(), config);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorListener.register();
+	}
+
+	@Override
+	protected void onPause() {
+		sensorListener.unregister();
+		super.onPause();
 	}
 }
