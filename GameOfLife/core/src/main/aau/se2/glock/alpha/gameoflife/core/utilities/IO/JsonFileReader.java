4824d300fda5f762b33ad5a3e5fe37c8180deb3c
lukita69 <122468559+lukita69@users.noreply.github.com>
2023-05-23 02:39:20 +0200

    Implemented JSON reader with Gdx library and class deserialization over dynamical class assignment

---
+++ GameOfLife/core/src/main/aau/se2/glock/alpha/gameoflife/core/utilities/IO/JsonFileReader.java
@@ -0,0 +1,24 @@
+package aau.se2.glock.alpha.gameoflife.core.utilities.IO;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+import java.util.ArrayList;
+
+public class JsonFileReader {
+
+        public <T> void readJson(String fileName, Class<T> type, JsonCallback<T> callback) {
+            Json json = new Json();
+            JsonReader reader = new JsonReader();
+            JsonValue base = reader.parse(Gdx.files.internal(fileName));
+
+            ArrayList<T> result = new ArrayList<>();
+            for (JsonValue val : base) {
+                result.add(json.readValue(type, val));
+            }
+
+            callback.onJsonRead(result);
+        }
+}
