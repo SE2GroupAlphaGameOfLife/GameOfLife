0a7e818824a48edf9c17e0f029ebe93d63bb9242
lukita69 <122468559+lukita69@users.noreply.github.com>
2023-06-05 10:38:23 +0200

    Renamed package to match convention

--- GameOfLife/core/src/main/aau/se2/glock/alpha/gameoflife/networking/observers/ClientObserver.java
+++ GameOfLife/core/src/main/aau/se2/glock/alpha/gameoflife/networking/observers/ClientObserver.java
@@ -1,4 +1,4 @@
-package aau.se2.glock.alpha.gameoflife.networking.Observers;
+package aau.se2.glock.alpha.gameoflife.networking.observers;
 
 public interface ClientObserver {
     void update(String payload);
