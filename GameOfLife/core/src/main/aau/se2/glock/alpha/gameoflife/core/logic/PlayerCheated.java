a31dea044fd7f428db4f544d2e50180138ba18d8
niwiunig <niwiunig@edu.aau.at>
2023-06-05 16:29:46 +0200

    Added Class PlayerCheated to save when Players have cheated

---
+++ GameOfLife/core/src/main/aau/se2/glock/alpha/gameoflife/core/logic/PlayerCheated.java
@@ -0,0 +1,25 @@
+package aau.se2.glock.alpha.gameoflife.core.logic;
+
+public class PlayerCheated {
+    private int cheatedAtAge;
+    private int playerId;
+    private int amountCheated;
+
+    public PlayerCheated(int cheatedAtAge, int playerId, int amountCheated){
+        this.amountCheated = amountCheated;
+        this.cheatedAtAge = cheatedAtAge;
+        this.playerId = playerId;
+    }
+
+    public int getCheatedAtAge() {
+        return cheatedAtAge;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public int getAmountCheated() {
+        return amountCheated;
+    }
+}
