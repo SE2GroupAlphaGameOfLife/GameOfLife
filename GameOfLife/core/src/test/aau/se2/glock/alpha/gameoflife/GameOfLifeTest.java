1c7395f654bd0ae751d311d57d53e878c2ae7eee
lukita69 <122468559+lukita69@users.noreply.github.com>
2023-06-13 04:46:54 +0200

    Updated GameOfLifeTest class for more coverage

--- GameOfLife/core/src/test/aau/se2/glock/alpha/gameoflife/GameOfLifeTest.java
+++ GameOfLife/core/src/test/aau/se2/glock/alpha/gameoflife/GameOfLifeTest.java
@@ -1,6 +1,7 @@
 package aau.se2.glock.alpha.gameoflife;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
@@ -8,7 +9,9 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.esotericsoftware.kryo.Kryo;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -17,8 +20,20 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import aau.se2.glock.alpha.gameoflife.core.Player;
+import aau.se2.glock.alpha.gameoflife.core.jobs.Job;
 import aau.se2.glock.alpha.gameoflife.core.utilities.ProximitySensorInterface;
 import aau.se2.glock.alpha.gameoflife.networking.client.ClientClass;
+import aau.se2.glock.alpha.gameoflife.networking.packages.CheatingMessage;
+import aau.se2.glock.alpha.gameoflife.networking.packages.DiscoveryResponsePacket;
+import aau.se2.glock.alpha.gameoflife.networking.packages.JoinedPlayers;
+import aau.se2.glock.alpha.gameoflife.networking.packages.ReportPlayerMessage;
+import aau.se2.glock.alpha.gameoflife.networking.packages.TcpMessage;
+import aau.se2.glock.alpha.gameoflife.screens.MainMenuScreen;
 
 public class GameOfLifeTest {
 
@@ -86,5 +101,52 @@ public class GameOfLifeTest {
 
         // Verify that the same instance is returned
         assertSame(firstInstance, secondInstance);
+    }
+
+    @Test
+    public void testRegisterClasses() {
+        // Arrange
+        Kryo mockKryo = Mockito.mock(Kryo.class);
+
+        // Act when isUnitTest is false
+        GameOfLife.registerClasses(mockKryo, false);
+
+        // Assert
+        verify(mockKryo).register(JoinedPlayers.class);
+        verify(mockKryo).register(Color.class);
+        verify(mockKryo).register(Player.class);
+        verify(mockKryo).register(Job.class);
+        verify(mockKryo).register(ArrayList.class);
+        verify(mockKryo).register(HashMap.class);
+        verify(mockKryo).register(DiscoveryResponsePacket.class);
+        verify(mockKryo).register(TcpMessage.class);
+        verify(mockKryo).register(ReportPlayerMessage.class);
+        verify(mockKryo).register(CheatingMessage.class);
+        verify(mockKryo).register(SecureRandom.class);
+
+        // Reset mockKryo
+        Mockito.reset(mockKryo);
+
+        // Act when isUnitTest is true
+        GameOfLife.registerClasses(mockKryo, true);
+
+        // Assert
+        verify(mockKryo).register(JoinedPlayers.class);
+        verify(mockKryo).register(Color.class);
+        verify(mockKryo).register(Player.class);
+        verify(mockKryo).register(Job.class);
+        verify(mockKryo).register(ArrayList.class);
+        verify(mockKryo).register(HashMap.class);
+        verify(mockKryo).register(DiscoveryResponsePacket.class);
+        verify(mockKryo).register(TcpMessage.class);
+        verify(mockKryo).register(ReportPlayerMessage.class);
+        verify(mockKryo).register(CheatingMessage.class);
+        verify(mockKryo, times(0)).register(SecureRandom.class);  // SecureRandom.class should not be registered when isUnitTest is true
+    }
+
+    @Test
+    public void testGetNewMainMenuScreen() {
+        MainMenuScreen screen = GameOfLife.getInstance().getNewMainMenuScreen();
+        assertNull(screen);
     }
 }
